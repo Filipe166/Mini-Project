5f27ef1ea99533e0decf9dc81f9a94044c3a6eb6
Ivan <isantos.santos255@gmail.com>
2021-04-20 15:11:54 +0200

    catalog work1.0

--- script/ajax_call.js
+++ script/ajax_call.js
@@ -1,14 +1,28 @@
+//load pag
 $(document).ready(function () {
-
     $.ajax({
         url: 'Modulo_php/catalog_q.php',
-        mathod: 'post',
-
+        dataType: 'json',
     })
         .done(function (result) {
-            $('#catalog').html(result)
+            // console.log('sussece' + result);
+            $.each(result, function (key, JsonCat) {
+                console.log(JsonCat);
+                $('#descri').append("<img src=" + JsonCat.post_products + " >")
+                $('#descri').append("<p> " + JsonCat.name_products + "</p>")
+                $('#descri').append("<p> " + JsonCat.relese_date_products + "</p>")
+                $('#descri').append("<p> " + JsonCat.discription_products + "</p>")
+                $('#descri').append("<p> " + JsonCat.price_products + "</p>")
+                $('#descri').append("<p> " + JsonCat.name_categories + "</p>")
+
+            })
         })
         .fail(function (result) {
-            console.log('fail');
+            console.log('Ajax fail' + result);
         })
+
 })
+
+// clik om az
+// clik om asc
+// clik om des
