07b703389bf9d47237547c88535ea0ee545023e9
Evguenia <evguenia.piriou@yahoo.fr>
2021-04-21 11:48:26 +0200

    Create home.php

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include_once 'nav.php'; ?>
+    <h1>Informatics</h1>
+
+    <p>
+        We will describe the website here, introduces the concept and firstly,
+        displays a large and wide search field, for searching among the products.
+    </p>
+
+
+</body>
+
+</html>
